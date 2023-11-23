9db102fd7b87631eb62c92e0c99aa2c260d97916
Nuno Trocado <ntrocado@gmail.com>
2023-11-23 20:00:43 +0000

    Add corpus analysis module

---
+++ corpus.lisp
@@ -0,0 +1,91 @@
+(in-package #:guitar-fx)
+
+(defsynth play-slice (buf start stop)
+  (out.ar 0 (dup (* (buf-rd.ar 1 buf (phasor.ar 0 (buf-rate-scale.ir buf) start stop) 0)
+		    (env-gen.kr (env '(0 1 1 0)
+				     (list .03 (- (/ (- stop start)
+						     (buf-sample-rate.ir buf))
+						  .06)
+					   .03))
+				:act :free)))))
+
+(defstruct slice
+  index
+  start
+  end
+  avg-centroid
+  max-loudness
+  norm-centroid
+  norm-loudness)
+
+(defparameter *src* nil)
+(defparameter *slices* nil)
+(defparameter *current-slice* nil)
+
+(defun play-slice (buf slice)
+  (unless (and *current-slice* (= (slice-index slice) *current-slice*))
+    (synth 'play-slice
+	   :buf buf
+	   :start (slice-start slice)
+	   :stop (min (+ (slice-start slice) (* 2 (sr buf)))
+		      (or (slice-end slice) (frames buf))))
+    (setf *current-slice* (slice-index slice))))
+
+(defun sequence->osc-string (seq)
+  (format nil "[~{~,4f~^,~}]" (coerce seq 'list)))
+
+(defun clear-canvas ()
+  (send-osc-message "/canvasHelper" '(-1 -1)))
+
+(defun populate-canvas (&optional (slices *slices*))
+  (clear-canvas)
+  (loop :for sl :across slices
+	:do (send-osc-message "/canvasHelper" (list (slice-norm-centroid sl)
+						    (- 1 (slice-norm-loudness sl))))
+	:do (sleep .05)))
+
+(sc-osc:add-osc-responder *osc* "/corpus2d"
+    (lambda (&rest param)
+      (destructuring-bind (x y)
+	  param
+	(when (and (zerop x) (zerop y))
+	  (setf *current-slice* nil))
+	(play-slice *src* (find (nearest x y) *slices* :key #'slice-index)))))
+
+(defun fill-kdtree (slices)
+  (let ((kd (kdtree-jk:build-kdtree 2 :npoints 0)))
+    (loop :for sl :across slices
+	  :do (kdtree-jk:insert-2d kd
+				   (slice-norm-centroid sl)
+				   (slice-norm-loudness sl)
+				   (slice-index sl)))
+    kd))
+
+(defparameter *kd*
+  (when *slices* (fill-kdtree *slices*)))
+
+(defun nearest (x y)
+  (elt (kdtree-jk:kdresult-obj-vec
+	(kdtree-jk:kd-find-k-nearest *kd*
+				     (make-array 2 :element-type 'double-float
+						 :initial-contents (mapcar (lambda (x)
+									     (coerce x 'double-float))
+									   (list x y)))
+				     1))
+       0))
+
+(defun load-analysis (path)
+  (destructuring-bind (src slices)
+      (uiop:with-safe-io-syntax (:package :guitar-fx)
+	(uiop:read-file-forms path))
+    (setf *src* (buffer-read-channel src :channels 0)
+	  *slices* slices
+	  *kd* (fill-kdtree *slices*))
+    (populate-canvas)))
+
+(sc-osc:add-osc-responder *osc* "/load_analysis"
+    (lambda (&rest param)
+      (ecase (truncate (car param))
+	(1 (load-analysis #p"~/OneDrive/Documents/Lisp/cl-collider/flucoma/example-analysis"))
+	(2 (load-analysis #p"~/OneDrive/Documents/Lisp/cl-collider/flucoma/bm-analysis"))
+	(3 (load-analysis #p"~/OneDrive/Documents/Lisp/cl-collider/guitar-fx/vilnius-analysis")))))
--- guitar-fx.asd
+++ guitar-fx.asd
@@ -1,11 +1,14 @@
 ;;;; Guitar-FX.asd
 
 (asdf:defsystem #:Guitar-fx
-  :description "Some effects that I use with guitar."
+  :description "Guitar + live electronics setup."
   :author "Nuno Trocado"
   :license  "Specify license here"
   :version "0.0.1"
   :serial t
-  :depends-on (#:alexandria #:sc-setup)
+  :depends-on (#:alexandria #:sc-setup #:sc-vst #:kdtree-jk)
   :components ((:file "package")
-               (:file "guitar-fx")))
+               (:file "guitar-fx")
+	       (:file "corpus")
+	       ;; (:file "vst")
+	       ))
