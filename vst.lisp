8f112b5b2ab84cbef7b3682011baa3b1c247bad4
Nuno Trocado <ntrocado@gmail.com>
2023-12-21 23:02:43 +0000

    Ignore error when s-gear not installed

--- vst.lisp
+++ vst.lisp
@@ -5,10 +5,15 @@
 (defsynth s-gear ()
   (out.ar 0 (sc-vst:vst-plugin.ar (sound-in.ar 1) 2 :s-gear)))
 
-(defparameter *s-gear* (sc-vst:vst-controller (synth 's-gear :to *vst-group*) :s-gear "S-Gear3_x64"))
+(defparameter *s-gear*
+  (ignore-errors
+   (sc-vst:vst-controller (synth 's-gear :to *vst-group*)
+			  :s-gear
+			  "S-Gear3_x64")))
 
 (defun s-gear-open-editor ()
-  (sc-vst:editor *s-gear*))
+  (when *s-gear*
+    (sc-vst:editor *s-gear*)))
 
 (sc-osc:add-osc-responder
     *osc*
